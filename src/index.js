f94dbb2ae65221064414403473eeafcbf5ef382e
sydorenkorv <99117460+sydorenkorv@users.noreply.github.com>
2022-12-30 15:30:50 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+
+
+
+// hero
+
+//alphabet
+const selectMobileEl = document.querySelector("#letter");
+const selectWidescreenEl = document.querySelector("#letter-widescreen");
+
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+for (let letter of alphabet) {
+  //mobile
+  let optionMobileEl = document.createElement("option");
+  optionMobileEl.value = letter.toLowerCase();
+  optionMobileEl.textContent = letter;
+    selectMobileEl.appendChild(optionMobileEl);
+  optionMobileEl.classList.add("hero__option")
+  //other
+
+  let optionWidescreenEl = document.createElement("li");
+  let optionWidescreenLink = document.createElement("button");
+  optionWidescreenLink.value = letter.toLowerCase();
+  optionWidescreenLink.textContent = letter;
+  selectWidescreenEl.appendChild(optionWidescreenEl);
+  optionWidescreenEl.appendChild(optionWidescreenLink)
+  optionWidescreenEl.classList.add("hero__alphabets-item")
+  optionWidescreenLink.classList.add("hero__alphabets-button", "button")
+}
