d6546ae6d140179a53916d9a5e78aaf0f9c21537
Andrii-Posternak <panposternak@gmail.com>
2023-01-03 22:17:32 +0200

    add hero select

--- src/index.js
+++ src/index.js
@@ -1,26 +1,87 @@
-// hero
+// // hero
+
+// //alphabet
+// const selectMobileEl = document.querySelector('#letter');
+// const selectWidescreenEl = document.querySelector('#letter-widescreen');
+
+// const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+
+// for (let letter of alphabet) {
+//   //mobile
+//   let optionMobileEl = document.createElement('option');
+//   optionMobileEl.value = letter.toLowerCase();
+//   optionMobileEl.textContent = letter;
+//   selectMobileEl.appendChild(optionMobileEl);
+//   optionMobileEl.classList.add('hero__option');
+//   //other
+
+//   let optionWidescreenEl = document.createElement('li');
+//   let optionWidescreenLink = document.createElement('button');
+//   optionWidescreenLink.value = letter.toLowerCase();
+//   optionWidescreenLink.textContent = letter;
+//   selectWidescreenEl.appendChild(optionWidescreenEl);
+//   optionWidescreenEl.appendChild(optionWidescreenLink);
+//   optionWidescreenEl.classList.add('hero__alphabets-item');
+//   optionWidescreenLink.classList.add('hero__alphabets-button', 'button');
+
+//   // optionWidescreenLink.setAttribute('type', 'button');
+// }
 
 //alphabet
-const selectMobileEl = document.querySelector("#letter");
-const selectWidescreenEl = document.querySelector("#letter-widescreen");
+const selectMobileEl = document.querySelector('.hero__select-list');
+const selectValue = document.querySelector('.hero__select-choose');
+const selectOptions = document.querySelector('.js-select-options');
+const selectWidescreenEl = document.querySelector('#letter-widescreen');
 
-const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+
+const optionsMobileElements = [];
+const optionsWidescreenElements = [];
 
 for (let letter of alphabet) {
   //mobile
-  let optionMobileEl = document.createElement("option");
-  optionMobileEl.value = letter.toLowerCase();
+  let optionMobileEl = document.createElement('li');
+  optionMobileEl.classList.add('hero__select-option');
+  optionMobileEl.setAttribute('value', letter.toLowerCase());
   optionMobileEl.textContent = letter;
-    selectMobileEl.appendChild(optionMobileEl);
-  optionMobileEl.classList.add("hero__option")
+  optionsMobileElements.push(optionMobileEl);
+
   //other
 
-  let optionWidescreenEl = document.createElement("li");
-  let optionWidescreenLink = document.createElement("button");
+  let optionWidescreenEl = document.createElement('li');
+  let optionWidescreenLink = document.createElement('button');
+  optionWidescreenEl.classList.add('hero__alphabets-item');
+  optionWidescreenLink.classList.add('hero__alphabets-button', 'button');
+  // optionWidescreenLink.setAttribute('type', 'button');
   optionWidescreenLink.value = letter.toLowerCase();
   optionWidescreenLink.textContent = letter;
-  selectWidescreenEl.appendChild(optionWidescreenEl);
-  optionWidescreenEl.appendChild(optionWidescreenLink)
-  optionWidescreenEl.classList.add("hero__alphabets-item")
-  optionWidescreenLink.classList.add("hero__alphabets-button", "button")
+  optionWidescreenEl.appendChild(optionWidescreenLink);
+  optionsWidescreenElements.push(optionWidescreenEl);
 }
+selectMobileEl.append(...optionsMobileElements);
+selectWidescreenEl.append(...optionsWidescreenElements);
+
+selectValue.addEventListener('click', onToggleSelectOptions);
+
+function onToggleSelectOptions() {
+  selectOptions.classList.toggle('is-hidden');
+}
+
+selectMobileEl.addEventListener('click', onOptionClick);
+
+function onOptionClick(e) {
+  selectValue.firstElementChild.textContent = e.target.textContent;
+}
+
+// function createMarkup() {
+//   const markup = alphabet
+//     .split('')
+//     .map(letter => {
+//       return `<li class="hero__select-option" value="${letter.toLowerCase()}">
+//     ${letter}
+//   </li>`;
+//     })
+//     .join('');
+//   selectMobileEl.insertAdjacentHTML('beforeend', markup);
+// }
+// createMarkup();
