d9a3631e6bdd49aab55c44d3a905c6f43900296a
sydorenkorv <99117460+sydorenkorv@users.noreply.github.com>
2023-01-05 09:36:54 +0000

    Update renderGallery.js

--- src/js/render/renderGallery.js
+++ src/js/render/renderGallery.js
@@ -2,7 +2,7 @@
 const apiURL = 'https://thecocktaildb.com/api/json/v1/1'
 
 
-export async function getData(letter){
+export async function getData(){
 	const response = await fetch(`${apiURL}/search.php?f=a`);
 	const cocktail = await response.json()
 	drinksData = cocktail.drinks
