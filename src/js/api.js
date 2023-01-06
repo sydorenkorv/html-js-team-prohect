1875badb80b5828fd95f911a18bd0dd19d53a917
sydorenkorv <99117460+sydorenkorv@users.noreply.github.com>
2023-01-06 16:46:53 +0000

    fixed DD renderer

--- src/js/api.js
+++ src/js/api.js
@@ -14,9 +14,21 @@ window.onload = getRandom()
 export let drinksData = []
 console.log(drinksData)
 
+// render by letter mobile
+
+const selectDdText = document.querySelector('.hero__select-list')
+selectDdText.onclick = async function(event) {
+    var target = event.target;
+    letter = event.target.innerHTML.toLowerCase();
+            await getData(letter)
+       await renderGallery()
+            await renderButtons()
+};  
+
+
+///////////render by letter WS
 
 const searchAlphabet = document.querySelectorAll('.hero__alphabets-button')
-
 
     for (var i = 0; i < searchAlphabet.length; i++) {
   
@@ -31,10 +43,16 @@ const searchAlphabet = document.querySelectorAll('.hero__alphabets-button')
 
 }
 
+
     
 async function getData(letter){
 	const response = await fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`);
 	const cocktail = await response.json()
 	drinksData = cocktail.drinks
 }
+
+
+
+
+
 
