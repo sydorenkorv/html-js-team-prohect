1f19325610586cc6cf4dd2486412b46614df9d34
sydorenkorv <99117460+sydorenkorv@users.noreply.github.com>
2023-01-11 10:45:29 +0000

    fixed modals

--- src/js/modeToggle.js
+++ src/js/modeToggle.js
@@ -1,4 +1,6 @@
 let darkToggle = document.querySelector('#switch');
+let modalIngr = document.querySelector('.modal-ingredient')
+let modalW = document.querySelector('.modal')
 
 
 if (sessionStorage.getItem("mode") == "dark") {
@@ -19,6 +21,8 @@ function darkmode() {
   document.body.classList.toggle('dark');
   darkToggle.checked = true;
   sessionStorage.setItem("mode", "dark");
+  modalIngr.style.backgroundColor = "#202025"
+  modalW.style.backgroundColor = "#202025"
 }
  
 
@@ -26,4 +30,6 @@ function lightmode() {
   document.body.classList.remove("dark");
   darkToggle.checked = false;
   sessionStorage.setItem("mode", "light")
+     modalIngr.removeAttribute("style")
+   modalW.removeAttribute("style")
 }
